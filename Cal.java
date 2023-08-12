da4c59eef976b0a6e354319e10249f7d19fb8094
roshanvarma007 <roshanvarma352@gmail.com>
2023-08-12 13:56:31 +0530

    Persentage Cal

---
+++ Cal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Cal {
+    public static void main(String[] args) {
+     float marks = 500;
+     float total = 600;
+
+     float perSentage = marks/total*100;
+        System.out.println(perSentage);
+
+    }
+}
